0fb6b8d1391351f8293581058ec9f2f297c74648
CandyTale55 <candytale55@gmail.com>
2025-11-22 23:22:08 +0100

    feat: add burger menu functionality (toggles the nav bar in mobile)

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 // Main JavaScript
 
 console.log('Mercedes Landing Page Loaded');
+
+const get = element => document.querySelector(element);
+
+let burgerMenu = get(".burger-menu");
+let mainNav = get(".main-nav-list");
+let menuClose = get(".menu-close");
+
+burgerMenu.addEventListener("click", () => {
+    mainNav.style.display
+})
